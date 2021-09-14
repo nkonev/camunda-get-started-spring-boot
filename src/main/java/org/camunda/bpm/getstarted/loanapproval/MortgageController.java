4737e6a28013f192475bb80246d69a8100089b40
Nikita Konev <nikit.cpp@yandex.ru>
2021-09-14 20:20:48 +0300

    nitial spring security

---
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/MortgageController.java
@@ -0,0 +1,67 @@
+package org.camunda.bpm.getstarted.loanapproval;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+class CreateUserDto {
+    private String login;
+    private String password;
+    private MortgageApp app;
+
+    public CreateUserDto() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public MortgageApp getApp() {
+        return app;
+    }
+
+    public void setApp(MortgageApp app) {
+        this.app = app;
+    }
+}
+
+class MortgageApp {
+    private String property;
+
+    public MortgageApp() {
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+}
+
+@RestController("/mortgage")
+public class MortgageController {
+    @PostMapping
+    public void register(@RequestBody CreateUserDto createUserDto) {
+
+    }
+
+    @GetMapping
+    public MortgageApp get() {
+        return new MortgageApp();
+    }
+}
